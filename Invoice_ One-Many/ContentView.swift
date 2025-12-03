b3730da4457fcc19522fef5624e5e25c39ea6471
Terje Moe <terjemoe54@hotmail.com>
2025-12-03 16:23:18 +0100

    Initial Commit

---
+++ Invoice_ One-Many/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Invoice_ One-Many
+//
+//  Created by Terje Moe on 03/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Invoice_ One-Many/Invoice__One_ManyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Invoice__One_ManyApp.swift
+//  Invoice_ One-Many
+//
+//  Created by Terje Moe on 03/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct Invoice__One_ManyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
